bf2cc9970c5a0a1aadc537358d23027fbf91215b
PeterMerrick6 <petermerrick6@gmail.com>
2026-06-01 23:43:54 -0600

    remove user self signup

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,14 @@
+import { RemovalPolicy } from "aws-cdk-lib";
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+backend.auth.resources.userPool.applyRemovalPolicy(RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE);
+backend.auth.resources.cfnResources.cfnUserPool.adminCreateUserConfig = {
+  allowAdminCreateUserOnly: true,
+};
